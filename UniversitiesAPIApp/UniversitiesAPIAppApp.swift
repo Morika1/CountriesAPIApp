8c710a7e2e0b3027b88905c9f60203cc3b5214c8
Morika1 <mor7025@gmail.com>
2023-12-07 14:36:19 +0200

    Initial Commit

---
+++ UniversitiesAPIApp/UniversitiesAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversitiesAPIAppApp.swift
+//  UniversitiesAPIApp
+//
+//  Created by Mac on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct UniversitiesAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
